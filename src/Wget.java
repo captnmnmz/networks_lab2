a89b28a62d82ccc95a888af760a238b682843ad3
Oliver Yates <oliver.yates01@gmail.com>
2017-09-28 16:31:40 +0200

    Implemented doIterative
    No tests done yet

--- src/Wget.java
+++ src/Wget.java
@@ -10,6 +10,10 @@ public class Wget {
 			// this method is called for each matched url
 			public void takeUrl(String url) {
 				// to be completed
+				if (!seen.contains(url)) {
+					queue.enqueue(url);
+					seen.add(url);
+				}
 			}
 		};
 		// to start, we push the initial url into the queue
