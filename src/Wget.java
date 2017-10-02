6e0faebee2a3f706c6be5ceb3068a5582ca20758
bastienchevallier <bastienchevallier@192.168.0.10>
2017-10-03 01:58:52 +0200

    doMultiThread and SynchronizedListQueue started but not finished,

--- src/Wget.java
+++ src/Wget.java
@@ -1,6 +1,55 @@
 import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.lang.Thread;
 
-public class Wget {
+class URLThread implements Runnable{
+	String requestedURL;
+	String proxyHost; 
+	int proxyPort;
+	
+	public URLThread(String requestedURL, String proxyHost,int proxyPort) {
+		this.requestedURL=  requestedURL;
+		this.proxyHost = proxyHost;
+		this.proxyPort = proxyPort;
+	}
+	public void run() {
+		Xurl.query(requestedURL, proxyHost, proxyPort);
+	};
+}
+
+public class Wget {	
+	
+	public static void doMultiThread(String requestedURL, String proxyHost,
+			int proxyPort) {
+		final URLQueue queue = new SynchronizedListQueue();
+		final HashSet<String> seen = new HashSet<String>();
+		// Synchronized
+		URLprocessing.handler = new URLprocessing.URLhandler() {
+			// this method is called for each matched url
+			public synchronized void takeUrl(String url) {
+				// Add a condition to wait
+				if (!seen.contains(url)) {
+					queue.enqueue(url);
+					seen.add(url);
+				}
+			}
+		};
+		// to start, we push the initial url into the queue
+		URLprocessing.handler.takeUrl(requestedURL);
+		while (Thread.activeCount()!=0) {
+			try {
+				String url = queue.dequeue();
+				URLThread url_thread = new URLThread(url, proxyHost, proxyPort);
+				Thread _thread = new Thread(url_thread);
+				_thread.start();
+			}catch(NoSuchElementException e) {
+				System.err.println(e.getMessage());
+			}
+
+		}
+		
+	}
+	
 
 	public static void doIterative(String requestedURL, String proxyHost,
 			int proxyPort) {
